7d046830bdb10ef6c330e64293f83691f0415347
kishor <kishorrda6@gmail.com>
2024-06-02 00:14:23 +0530

    update

--- megaproject/src/components/index.js
+++ megaproject/src/components/index.js
@@ -1,6 +1,13 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Container from "./Container"
-import Logo from "./Logo"
+import Logo from "./Logo";
+import LogoutBtn from "./LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from './Login';
+import Post from './Post'
+import PostForm from '../components/Post-form'
+import PostCard from '../components'
 
-export {Header, Footer, Container, Logo}
+export {Header, Footer, Container, Logo, LogoutBtn, RTE , Signup, Login, Post, PostForm, PostCard}
